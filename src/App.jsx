6098d6c8ed16279cf59cc6ffae8bb04a8f3ffd23
Yen Pinero Ferro <ypine022@fiu.edu>
2022-04-01 10:19:48 -0400

    commit on 'master'

--- src/App.jsx
+++ src/App.jsx
@@ -120,16 +120,28 @@ function App() {
           id="about"
         >
           <div className="w-full h-420 flex items-center justify-center">
-            <div className="w-275 h-340 bg-emerald-200 rounded-md">
+            <div className="w-275 h-340 bg-emerald-200 rounded-md relative">
               <img
-                className="w-full h-full absolute -right-4 top-4 object-cover rounded-lg drop-shadow-2xl"
+                className="w-full h-full absolute -right-4 top-4 object-cover rounded-lg shadow-lg"
                 src={Yen}
                 alt=""
               />
             </div>
           </div>
-          
-          <div className=""></div>
+
+          <div className="w-full h-420 flex flex-col items-center justify-center">
+            <p className="  text-lg text-textBase text-center">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+              qui, quasi dicta facilis necessitatibus doloribus! Aut, libero
+              voluptate! Excepturi itaque provident officiis repellendus
+              voluptate labore obcaecati praesentium aperiam delectus sed.
+            </p>
+            <button class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
+              <span class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Download
+              </span>
+            </button>
+          </div>
         </section>
       </main>
     </div>
