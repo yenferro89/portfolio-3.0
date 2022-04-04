01e08acbe32e7a21b6ad4c2c07ce5653e80626a1
Yen Pinero Ferro <ypine022@fiu.edu>
2022-04-04 12:48:43 -0400

    commit on 'master'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { IoMenu, IoLogoGithub } from "react-icons/io5";
 import Spline from "@splinetool/react-spline";
 import { useState } from "react";
-import { Experience, Projects } from "./data";
+import { Experience, Projects, SocialLinks } from "./data";
 
 import {
   VerticalTimeline,
@@ -11,225 +11,244 @@ import "react-vertical-timeline-component/style.min.css";
 
 import Yen from "./images/IMG_0635.JPG";
 
+import { AnimatePresence, motion } from "framer-motion";
+
 function App() {
   const [isActive, setIsActive] = useState(false);
 
   return (
-    <div className="flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
-      {/* Navigation Bar*/}
-      <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
-        <div className="w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center">
-          <p className="text-lg text-slate-200 font-medium">Yen Ferro</p>
-          <div className="hidden md:flex items-center gap-6 ml-6 flex-1">
-            <a
-              href="#home"
-              className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
-            >
-              Home
-            </a>
-            <a
-              href="#about"
-              className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
-            >
-              About
-            </a>
-            <a
-              href="#projects"
-              className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
-            >
-              Projects
-            </a>
-            <a
-              href="#contact"
-              className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
-            >
-              Contact
-            </a>
-            <a
-              href="#"
-              className="ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out"
-            >
-              Download
-            </a>
-          </div>
-          <div
-            className="block md:hidden ml-auto cursor-pointer"
-            onClick={() => setIsActive(!isActive)}
-          >
-            <IoMenu className="text-2xl text-textBase"></IoMenu>
-          </div>
-
-          {isActive && (
-            <div className="p-4 w-275 bg-navBar rounded-lg fixed top-24 right-16 flex flex-col items-center justify-evenly gap-6">
+    <AnimatePresence initial={false}>
+      <div className="flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
+        {/* Navigation Bar*/}
+        <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
+          <div className="w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center">
+            <p className="text-lg text-slate-200 font-medium">Yen Ferro</p>
+            <div className="hidden md:flex items-center gap-6 ml-6 flex-1">
               <a
                 href="#home"
                 className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
-                onClick={() => {
-                  setIsActive(false);
-                }}
               >
                 Home
               </a>
               <a
                 href="#about"
                 className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
-                onClick={() => {
-                  setIsActive(false);
-                }}
               >
                 About
               </a>
               <a
                 href="#projects"
                 className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
-                onClick={() => {
-                  setIsActive(false);
-                }}
               >
                 Projects
               </a>
               <a
                 href="#contact"
                 className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
-                onClick={() => {
-                  setIsActive(false);
-                }}
               >
                 Contact
               </a>
               <a
                 href="#"
-                className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out"
-                onClick={() => {
-                  setIsActive(false);
-                }}
+                className="ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out"
               >
                 Download
               </a>
             </div>
-          )}
-        </div>
-      </nav>
+            <motion.div 
+              whileTap={{scale:0.5}}
+              className="block md:hidden ml-auto cursor-pointer"
+              onClick={() => setIsActive(!isActive)}
+            >
+              <IoMenu className="text-2xl text-textBase"></IoMenu>
+            </motion.div>
 
-      <div className="relative" id="home">
-        <Spline scene="https://draft.spline.design/oCVA7B0VkZebUG0a/scene.spline" />
-        <div className="absolute bottom-10 w-full justify-center items-center flex">
-          <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl">
-            <p className="text-textBase">Press and drag to orbit</p>
+            {isActive && (
+              <motion.div
+              initial={{opacity:0, scale: 0.5}}
+              animate={{opacity:1, scale: 1.1}}
+              exit={{opacity:0, scale:0.5}}
+              transition={{delay: "0.1s", type: "spring"}}
+              className="p-4 w-275 bg-navBar rounded-lg fixed top-24 right-16 flex flex-col items-center justify-evenly gap-6">
+                <a
+                  href="#home"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
+                  onClick={() => {
+                    setIsActive(false);
+                  }}
+                >
+                  Home
+                </a>
+                <a
+                  href="#about"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
+                  onClick={() => {
+                    setIsActive(false);
+                  }}
+                >
+                  About
+                </a>
+                <a
+                  href="#projects"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
+                  onClick={() => {
+                    setIsActive(false);
+                  }}
+                >
+                  Projects
+                </a>
+                <a
+                  href="#contact"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out"
+                  onClick={() => {
+                    setIsActive(false);
+                  }}
+                >
+                  Contact
+                </a>
+                <a
+                  href="#"
+                  className="text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out"
+                  onClick={() => {
+                    setIsActive(false);
+                  }}
+                >
+                  Download
+                </a>
+              </motion.div>
+            )}
           </div>
-        </div>
-      </div>
+        </nav>
 
-      {/*Main Section*/}
-      <main className="w-[80%] mt-5">
-        <section
-          className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24"
-          id="about"
-        >
-          <div className="w-full h-420 flex items-center justify-center">
-            <div className="w-275 h-340 bg-emerald-200 rounded-md relative">
-              <img
-                className="w-full h-full absolute -right-4 top-4 object-cover rounded-lg shadow-lg"
-                src={Yen}
-                alt=""
-              />
+        <div className="relative" id="home">
+          <Spline scene="https://draft.spline.design/oCVA7B0VkZebUG0a/scene.spline" />
+          <div className="absolute bottom-10 w-full justify-center items-center flex">
+            <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl">
+              <p className="text-textBase">Press and drag to orbit</p>
             </div>
           </div>
+        </div>
 
-          <div className="w-full h-420 flex flex-col items-center justify-center">
-            <p className="  text-lg text-textBase text-center">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
-              qui, quasi dicta facilis necessitatibus doloribus! Aut, libero
-              voluptate! Excepturi itaque provident officiis repellendus
-              voluptate labore obcaecati praesentium aperiam delectus sed.
-            </p>
-            <button class="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
-              <span class="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                Download
-              </span>
-            </button>
-          </div>
-        </section>
-
-        {/*Timeline*/}
-        <section className="w-full flex items-center justify-start">
-          <VerticalTimeline>
-            {Experience &&
-              Experience.map((n) => (
-                <VerticalTimelineElement
-                  key={n.id}
-                  className="vertical-timeline-element--work"
-                  contentStyle={{ background: "rgb(21,24,31)", color: "#888" }}
-                  contentArrowStyle={{
-                    borderRight: "7px solid  rgb(21,24,31)",
-                  }}
-                  date="2011 - present"
-                  iconStyle={{ background: "rgb(21,24,31)", color: "#888" }}
-                  icon={n.iconsSrc}
-                >
-                  <h3 className="vertical-timeline-element-title">{n.title}</h3>
-                  <h4 className="vertical-timeline-element-subtitle">
-                    {n.location}
-                  </h4>
-                  <p>{n.description}</p>
-                </VerticalTimelineElement>
-              ))}
-          </VerticalTimeline>
-        </section>
-
-        {/*Projects Section*/}
-        <section
-          className="  flex flex-wrap items-center justify-evenly my-24 gap-4"
-          id="projects"
-        >
-          {Projects &&
-            Projects.map((n) => (
-              <div
-                key={n.id}
-                className="  border border-zinc-800 rounded-md p-2 min-w-[27px] md:max-w-[275px] hover:border-zinc-600 duration-100 ease-in-out"
-              >
-                <p className=" text-md text-textBase font-medium uppercase">
-                  {n.name.length > 25 ? `${n.name.slice(0, 25)}...` : n.name}
-                </p>
+        {/*Main Section*/}
+        <main className="w-[80%] mt-5">
+          <section
+            className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24"
+            id="about"
+          >
+            <div className="w-full h-420 flex items-center justify-center">
+              <div className="w-275 h-340 bg-emerald-200 rounded-md relative">
                 <img
-                  src={n.imageSrc}
-                  className=" w-full h-full object-cover rounded-md my-4"
+                  className="w-full h-full absolute -right-4 top-4 object-cover rounded-lg shadow-lg"
+                  src={Yen}
+                  alt=""
                 />
-                <div className="flex flex-1 items-center justify-between">
-                  <p className="text-lg text-gray-300">
-                    Technologies
-                    <span className="block text-sm text-gray-500">
-                      {n.techs}
-                    </span>
-                  </p>
-                  <a href={n.github} alt="" className="">
-                    <div className="">
-                      <IoLogoGithub className=" text-textBase text-3xl cursor-pointer"></IoLogoGithub>
-                    </div>
-                  </a>
-                </div>
               </div>
-            ))}
-        </section>
+            </div>
 
-        <section
-          className="flex flex-col items-center justify-center w-full my-24"
-          id="contact"
-        >
-          <p className=" text-2xl text-gray-400 capitalize">Follow me</p>
-          <div className="flex items-center justify-center w-full my-4 flex-wrap gap-4">
-            <a
-              href="#"
-              className="w-full md:w-auto px-3 md:px-8 py-5 border border-zinc-800 rounded-2xl hover:border-zinc-600 duration-100 ease-in-out cursor-pointer flex items-center justify-center gap-3
+            <div className="w-full h-420 flex flex-col items-center justify-center">
+              <p className="  text-lg text-textBase text-center">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Ratione qui, quasi dicta facilis necessitatibus doloribus! Aut,
+                libero voluptate! Excepturi itaque provident officiis
+                repellendus voluptate labore obcaecati praesentium aperiam
+                delectus sed.
+              </p>
+              <button className="w-full md:w-auto relative mt-6 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg hover:dark:shadow-teal-800/80">
+                <span className="w-full md:w-auto relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Download
+                </span>
+              </button>
+            </div>
+          </section>
+
+          {/*Timeline*/}
+          <section className="w-full flex items-center justify-start">
+            <VerticalTimeline>
+              {Experience &&
+                Experience.map((n) => (
+                  <VerticalTimelineElement
+                    key={n.id}
+                    className="vertical-timeline-element--work"
+                    contentStyle={{
+                      background: "rgb(21,24,31)",
+                      color: "#888",
+                    }}
+                    contentArrowStyle={{
+                      borderRight: "7px solid  rgb(21,24,31)",
+                    }}
+                    date="2011 - present"
+                    iconStyle={{ background: "rgb(21,24,31)", color: "#888" }}
+                    icon={n.iconsSrc}
+                  >
+                    <h3 className="vertical-timeline-element-title">
+                      {n.title}
+                    </h3>
+                    <h4 className="vertical-timeline-element-subtitle">
+                      {n.location}
+                    </h4>
+                    <p>{n.description}</p>
+                  </VerticalTimelineElement>
+                ))}
+            </VerticalTimeline>
+          </section>
+
+          {/*Projects Section*/}
+          <section
+            className="  flex flex-wrap items-center justify-evenly my-24 gap-4"
+            id="projects"
+          >
+            {Projects &&
+              Projects.map((n) => (
+                <div
+                  key={n.id}
+                  className="  border border-zinc-800 rounded-md p-2 min-w-[27px] md:max-w-[275px] hover:border-zinc-600 duration-100 ease-in-out"
+                >
+                  <p className=" text-md text-textBase font-medium uppercase">
+                    {n.name.length > 25 ? `${n.name.slice(0, 25)}...` : n.name}
+                  </p>
+                  <img
+                    src={n.imageSrc}
+                    className=" w-full h-full object-cover rounded-md my-4"
+                  />
+                  <div className="flex flex-1 items-center justify-between">
+                    <p className="text-lg text-gray-300">
+                      Technologies
+                      <span className="block text-sm text-gray-500">
+                        {n.techs}
+                      </span>
+                    </p>
+                    <a href={n.github} alt="" className="">
+                      <div className="">
+                        <IoLogoGithub className=" text-textBase text-3xl cursor-pointer"></IoLogoGithub>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              ))}
+          </section>
+
+          <section
+            className="flex flex-col items-center justify-center w-full my-24"
+            id="contact"
+          >
+            <p className=" text-2xl text-gray-400 capitalize">Follow me</p>
+            <div className="flex items-center justify-center w-full my-4 flex-wrap gap-4">
+              {SocialLinks &&
+                SocialLinks.map((n) => (
+                  <a
+                    href="#"
+                    className="w-full md:w-auto px-3 md:px-8 py-5 border border-zinc-800 rounded-2xl hover:border-zinc-600 duration-100 ease-in-out cursor-pointer flex items-center justify-center gap-3
             "
-            >
-              <IoLogoGithub className="text-textBase text-3xl cursor-pointer" />
-              <p className=" text-lg text-textBase">Github</p>
-            </a>
-          </div>
-        </section>
-      </main>
-    </div>
+                  >
+                    {n.iconSrc}
+                    <p className=" text-lg text-textBase">{n.name}</p>
+                  </a>
+                ))}
+            </div>
+          </section>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 }
 
--- src/data.jsx
+++ src/data.jsx
@@ -1,141 +1,141 @@
 import {
-    IoCodeWorking,
-    IoLogoGithub,
-    IoLogoYoutube,
-    IoLogoTwitter,
-    IoLogoLinkedin,
-    IoLogoWhatsapp,
-  } from "react-icons/io5";
-  import img1 from "./images/IMG_1339.jpg";
-    
-  export const Experience = [
-    {
-      id: 1,
-      date: "2016 - present",
-      iconsSrc: <IoCodeWorking />,
-      title: "Creative Director",
-      location: "Miami, FL",
-      description:
-        "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-    },
-    {
-      id: 2,
-      date: "2014 - 2016",
-      iconsSrc: <IoCodeWorking />,
-      title: "Creative Director",
-      location: "Miami, FL",
-      description:
-        "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-    },
-    {
-      id: 3,
-      date: "2012 - 2014",
-      iconsSrc: <IoCodeWorking />,
-      title: "Creative Director",
-      location: "Miami, FL",
-      description:
-        "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-    },
-    {
-      id: 4,
-      date: "2011 - 2012",
-      iconsSrc: <IoCodeWorking />,
-      title: "Creative Director",
-      location: "Miami, FL",
-      description:
-        "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-    },
-    {
-      id: 5,
-      date: "2010 - 2011",
-      iconsSrc: <IoCodeWorking />,
-      title: "Creative Director",
-      location: "Miami, FL",
-      description:
-        "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-    },
-  ];
-  
-  export const Projects = [
-    {
-      id: 1,
-      name: "Food Delivery UI Mockup",
-      imageSrc: img1,
-      techs: "React Js, Redux, Material UI",
-      github: "#",
-    },
-    {
-      id: 2,
-      name: "Full Stack Social Media App ",
-      imageSrc: img1,
-      techs: "React Js, Sanity, Tailwind CSS",
-      github: "#",
-    },
-    {
-      id: 3,
-      name: "Travel Advisor",
-      imageSrc: img1,
-      techs: "React Js, Chakra UI, Google API",
-      github: "#",
-    },
-    {
-      id: 4,
-      name: "Pintrest UI Clone 2.0 ",
-      imageSrc: img1,
-      techs: "React Js,  Material UI",
-      github: "#",
-    },
-    {
-      id: 5,
-      name: "Own Video Blog",
-      imageSrc: img1,
-      techs: "React Js, Chara UI, Firebase",
-      github: "#",
-    },
-    {
-      id: 6,
-      name: "Whatsapp UI Clone",
-      imageSrc: img1,
-      techs: "NextJs, Firebase Auth",
-      github: "#",
-    },
-  ];
-  
-  export const SocialLinks = [
-    {
-      id: 1,
-      iconSrc: <IoLogoGithub className="text-textBase text-3xl cursor-pointer" />,
-      name: "GitHub",
-      link: "#",
-    },
-    {
-      id: 1,
-      iconSrc: <IoLogoYoutube className="text-red-500 text-3xl cursor-pointer" />,
-      name: "YouTube",
-      link: "#",
-    },
-    {
-      id: 1,
-      iconSrc: (
-        <IoLogoTwitter className="text-blue-500 text-3xl cursor-pointer" />
-      ),
-      name: "Twitter",
-      link: "#",
-    },
-    {
-      id: 1,
-      iconSrc: (
-        <IoLogoLinkedin className="text-blue-800 text-3xl cursor-pointer" />
-      ),
-      name: "LinkedIn",
-      link: "#",
-    },
-    {
-      id: 1,
-      iconSrc: (
-        <IoLogoWhatsapp className="text-green-500 text-3xl cursor-pointer" />
-      ),
-      name: "Whatsapp",
-      link: "#",
-    },
-  ];
+  IoCodeWorking,
+  IoLogoGithub,
+  IoLogoYoutube,
+  IoLogoTwitter,
+  IoLogoLinkedin,
+  IoLogoWhatsapp,
+} from "react-icons/io5";
+import img1 from "./images/IMG_1339.jpg";
+
+export const Experience = [
+  {
+    id: 1,
+    date: "2016 - present",
+    iconsSrc: <IoCodeWorking />,
+    title: "Creative Director",
+    location: "Miami, FL",
+    description:
+      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+  },
+  {
+    id: 2,
+    date: "2014 - 2016",
+    iconsSrc: <IoCodeWorking />,
+    title: "Creative Director",
+    location: "Miami, FL",
+    description:
+      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+  },
+  {
+    id: 3,
+    date: "2012 - 2014",
+    iconsSrc: <IoCodeWorking />,
+    title: "Creative Director",
+    location: "Miami, FL",
+    description:
+      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+  },
+  {
+    id: 4,
+    date: "2011 - 2012",
+    iconsSrc: <IoCodeWorking />,
+    title: "Creative Director",
+    location: "Miami, FL",
+    description:
+      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+  },
+  {
+    id: 5,
+    date: "2010 - 2011",
+    iconsSrc: <IoCodeWorking />,
+    title: "Creative Director",
+    location: "Miami, FL",
+    description:
+      "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
+  },
+];
+
+export const Projects = [
+  {
+    id: 1,
+    name: "Food Delivery UI Mockup",
+    imageSrc: img1,
+    techs: "React Js, Redux, Material UI",
+    github: "#",
+  },
+  {
+    id: 2,
+    name: "Full Stack Social Media App ",
+    imageSrc: img1,
+    techs: "React Js, Sanity, Tailwind CSS",
+    github: "#",
+  },
+  {
+    id: 3,
+    name: "Travel Advisor",
+    imageSrc: img1,
+    techs: "React Js, Chakra UI, Google API",
+    github: "#",
+  },
+  {
+    id: 4,
+    name: "Pintrest UI Clone 2.0 ",
+    imageSrc: img1,
+    techs: "React Js,  Material UI",
+    github: "#",
+  },
+  {
+    id: 5,
+    name: "Own Video Blog",
+    imageSrc: img1,
+    techs: "React Js, Chara UI, Firebase",
+    github: "#",
+  },
+  {
+    id: 6,
+    name: "Whatsapp UI Clone",
+    imageSrc: img1,
+    techs: "NextJs, Firebase Auth",
+    github: "#",
+  },
+];
+
+export const SocialLinks = [
+  {
+    id: 1,
+    iconSrc: <IoLogoGithub className="text-textBase text-3xl cursor-pointer" />,
+    name: "GitHub",
+    link: "#",
+  },
+  {
+    id: 2,
+    iconSrc: <IoLogoYoutube className="text-red-500 text-3xl cursor-pointer" />,
+    name: "YouTube",
+    link: "#",
+  },
+  {
+    id: 3,
+    iconSrc: (
+      <IoLogoTwitter className="text-blue-500 text-3xl cursor-pointer" />
+    ),
+    name: "Twitter",
+    link: "#",
+  },
+  {
+    id: 4,
+    iconSrc: (
+      <IoLogoLinkedin className="text-blue-800 text-3xl cursor-pointer" />
+    ),
+    name: "LinkedIn",
+    link: "#",
+  },
+  {
+    id: 5,
+    iconSrc: (
+      <IoLogoWhatsapp className="text-green-500 text-3xl cursor-pointer" />
+    ),
+    name: "Whatsapp",
+    link: "#",
+  },
+];
