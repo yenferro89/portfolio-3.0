e8de89da29b8d203053480c5d2a6fbd5d19c0dd6
Yen Pinero Ferro <ypine022@fiu.edu>
2022-04-06 16:13:49 -0400

    commit on 'master'

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,9 @@ function App() {
         {/* Navigation Bar*/}
         <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
           <div className="w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center">
-            <p className="text-lg text-slate-200 font-medium">Yen Ferro</p>
+            <p className="text-lg text-slate-200 font-medium">
+              Yen Pinero Ferro
+            </p>
             <div className="hidden md:flex items-center gap-6 ml-6 flex-1">
               <a
                 href="#home"
@@ -48,12 +50,13 @@ function App() {
               >
                 Contact
               </a>
-              <a
+              <motion.a
+                whileTap={{ scale: 0.5 }}
                 href="#"
                 className="ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out"
               >
                 Download
-              </a>
+              </motion.a>
             </div>
             <motion.div
               whileTap={{ scale: 0.5 }}
@@ -222,9 +225,7 @@ function App() {
                       </span>
                     </p>
                     <a href={n.github} target="_blank">
-                      <motion.div 
-                      whileTap={{ scale: 0.8 }} 
-                      className="">
+                      <motion.div whileTap={{ scale: 0.8 }} className="">
                         <IoLogoGithub className=" text-textBase text-3xl cursor-pointer"></IoLogoGithub>
                       </motion.div>
                     </a>
