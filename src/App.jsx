7fca2d01519fb895b762cbda59bd08592ace2b99
Yen Pinero Ferro <ypine022@fiu.edu>
2022-04-14 10:17:18 -0400

    commit on 'master'

--- src/App.jsx
+++ src/App.jsx
@@ -132,7 +132,7 @@ function App() {
         </nav>
 
         <div className="relative" id="home">
-          <Spline scene="https://prod.spline.design/a1g0-oyl6xLI11Ba/scene.spline" />
+          <Spline scene="https://prod.spline.design/a1g0-oyl6xLI11Ba/scene.spline"/>
           <div className="absolute bottom-10 w-full justify-center items-center flex">
             <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl">
               <p className="text-textBase">Press and drag to orbit</p>
